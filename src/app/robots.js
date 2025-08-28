c62c5f2c090399463ef8ef13cc767220b20738fa
Lucasporter0 <lucasporter999@gmail.com>
2025-08-28 11:57:17 -0400

    Update robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,11 +1,12 @@
 // src/app/robots.js
-
 export default function robots() {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://naturaledgemedia.net/sitemap.xml",
-  };
+  const isProd = process.env.VERCEL_ENV === "production";
+  return isProd
+    ? {
+        rules: { userAgent: "*", allow: "/" },
+        sitemap: "https://naturaledgemedia.net/sitemap.xml",
+      }
+    : {
+        rules: { userAgent: "*", disallow: "/" },
+      };
 }
