b368110f7dab0d4b5c9390b29605a6827859d532
Lucasporter0 <lucasporter999@gmail.com>
2025-08-28 11:28:20 -0400

    Update robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,1 +1,13 @@
+// src/app/robots.js
+import { MetadataRoute } from "next";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://naturaledgemedia.net/sitemap.xml",
+  } satisfies MetadataRoute.Robots;
+}
 
